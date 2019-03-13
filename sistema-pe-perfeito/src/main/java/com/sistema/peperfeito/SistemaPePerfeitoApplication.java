1ce3a2023af3fb191360ba2744b89e8e5d2e7f19
alexd <alexd@192.168.10.121>
2019-03-13 17:59:54 -0300

    Primeiro commit

---
+++ sistema-pe-perfeito/src/main/java/com/sistema/peperfeito/SistemaPePerfeitoApplication.java
@@ -0,0 +1,13 @@
+package com.sistema.peperfeito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaPePerfeitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaPePerfeitoApplication.class, args);
+	}
+
+}
