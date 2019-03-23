e9c8fadf755c1915ce5fe4e4bb26cbc09145860e
alexd <alexd@192.168.10.113>
2019-03-23 11:46:54 -0300

    configurações externas

--- sistema-pe-perfeito/src/main/java/com/sistema/peperfeito/SistemaPePerfeitoApplication.java
+++ sistema-pe-perfeito/src/main/java/com/sistema/peperfeito/SistemaPePerfeitoApplication.java
@@ -2,8 +2,12 @@ package com.sistema.peperfeito;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import com.sistema.peperfeito.config.property.PePerfeitoProperty;
 
 @SpringBootApplication
+@EnableConfigurationProperties(PePerfeitoProperty.class)
 public class SistemaPePerfeitoApplication {
 
 	public static void main(String[] args) {
